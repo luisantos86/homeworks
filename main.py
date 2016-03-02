b039f2e6c8c2a46ee80e1ebffccbe2fa3ee77d61
luisantos86 <ing.luisantos@itesm.mx>
2016-03-02 12:47:17 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+def main():   
+
+    print "1. Solve the following exercises \n \na) Assume that the following variables have the next values:\n\nnumberRed = 100\nnumberBlue = 200 \nnumberGreen= 300\nwordRed = 'Car' \nwordBlue = 'Sky' \nwordGreen= 'Sand'\n"
+    
+    raw_input("press enter to contuinue...\n")
+    
+    print "For each of the following Boolean expressions, determine if the statement is true or false.\n"
+    print "numberRed = numberBlue"
+    print "numberBlue > numberGreen"
+    print "numberGreen < numberRed"
+    print "numberBlue = wordBlue"
+    print "numberGreen = 'Green'"
+    print "wordRed ='Red'"
+    print "wordBlue ='Blue'"
+    print "numberRed <=numberGreen"
+    print "numberBlue >=200"
+    print "numberGreen >= numberRed + numberBlue"
+    
+    raw_input("press enter to contuinue...\n")
+    
+    print "\nb) Assume that a = 2 and b = 3, determine if the condition is true or false.\n"
+    print "3 * a = 2 * b"
+    print "( 5 - a ) * b < 7"
+    print "a  b = b ** a"
+    print "(a < b ) OR (b < a)"
+    print "(a * a < b) OR Not ( a * a < a)"
+    print "Not ((a < b) AND (a < ( b + a)))"
+    print "Not (a < b) OR Not (a < (b + a))"
+    print "a = b OR b <> a AND b >= a "
+    
+    print "Solve the textbook's Chapter 2, Exercise 5: Hierarchy of operations." 
+    print "https://www.editorialdigitaltec.com/materialadicional/ida005/tablasyfiguras/c2/exercise5.pdf" 
+    
+if __name__ == '__main__':
+    main()
